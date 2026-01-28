cbaf6833da919eb23ec7d77e008b48302ac68d8b
Quintos <60225455+zeekofficial@users.noreply.github.com>
2026-01-28 17:51:27 +0100

    Update Firebase configuration with real credentials

--- firebase.js
+++ firebase.js
@@ -1,13 +1,18 @@
+// js/firebase.js
+
+// Firebase config (WEB APP - CDN)
 const firebaseConfig = {
-  apiKey: "PASTE_REAL_KEY_HERE",
-  authDomain: "PASTE_REAL_DOMAIN",
-  projectId: "PASTE_REAL_PROJECT_ID",
-  storageBucket: "PASTE_REAL_BUCKET",
-  messagingSenderId: "PASTE_REAL_SENDER_ID",
-  appId: "PASTE_REAL_APP_ID"
+  apiKey: "AIzaSyD9kwIwoIM1vKBx5KUZKj4V55TuV3EVm-A",
+  authDomain: "xpenditia.firebaseapp.com",
+  projectId: "xpenditia",
+  storageBucket: "xpenditia.appspot.com",
+  messagingSenderId: "765558500051",
+  appId: "1:765558500051:web:5dee5a10add207d3260226"
 };
 
+// Initialize Firebase (CDN COMPAT)
 firebase.initializeApp(firebaseConfig);
 
+// Services
 const auth = firebase.auth();
 const db = firebase.firestore();
